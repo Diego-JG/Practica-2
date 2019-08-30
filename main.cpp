0973942f40b30bec8046bf8d458afeea42838e02
Diego-JG <holasoydiegojg@gmail.com>
2019-08-30 13:15:52 -0500

    Utilizamos dobles estructuras y utilizamos apuntadores dentro de las estructuras

--- main.cpp
+++ main.cpp
@@ -1,20 +1,40 @@
+
 #include <bits/stdc++.h>
-#include <conio.h>
-
 using namespace std;
+struct Musica{
+    char nombre[22];
+    char cancion[22];
+};
+struct Cancion{
+    char nombre[22];
+    char albums;
+    Musica musica;
+};
 
-int main (int argc, char** argv){
-	int valores[10], *puntero;
-	puntero=&valores[0];
-	int mayor=INT_MIN;
-	for (int i=1; i<=10; i++){
-		cout<<"Dame el valor "<<i<<endl;
-		cin>>valores[i];	
-	}
-	for (int *puntero=&valores[1]; puntero<=&valores[10]; puntero++){
-		if(*puntero>mayor) mayor=*puntero;
-	}
-	cout<<"El valor mayor es: "<<mayor<<endl;
-		
-	return 0;
-	}
+int main()
+{
+    Cancion canciones[12];
+    Cancion *apuntador=canciones;
+    for(int i=0;i<=1;i++){
+        cout<<"Nombre del artista: ";
+        cin.getline(canciones[i].nombre,20);
+        cout<<"Nombre de la cancion: ";
+        cin>>canciones[i].albums;
+        cout<<"Genero: ";
+        fflush(stdin);
+        cin.getline(canciones[i].musica.nombre,20);
+        cout<<"Album: ";
+        cin.getline(canciones[i].musica.cancion,20);
+    }
+    cout<<endl;
+    for(;apuntador<=&canciones[1];apuntador++){
+        cout<<"Nombre del artista:  "<<apuntador->nombre<<endl;
+        cout<<"Nombre de la cancion: "<<apuntador->albums<<endl;
+        cout<<"Genero: "<<apuntador->musica.nombre<<endl;
+        cout<<"Album: "<<apuntador->musica.cancion<<endl;
+        cout<<endl;
+    }
+
+    return 0;
+}
+
