650c7652a7d349e48036e16b84ef73bbfa2b5b99
Diego-JG <holasoydiegojg@gmail.com>
2019-09-25 13:16:30 -0500

    Muestra un menu para altas, bajas y consulta de usuarios para una revista

--- main.cpp
+++ main.cpp
@@ -1,40 +1,95 @@
 
-#include <iostream>
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include<locale>
 using namespace std;
-struct Subscripcion{
-    char nombre[22];
-    char rev[22];
-};
-struct Revista{
-    char subs;
-    float costo;
-    Subscripcion subscripcion;
+
+struct persona{
+    char nombre[20];
+    int suscripcion;
+    char revista[20];
 };
 
-int main()
-{
-    Revista revistas[12];
-    Revista *apuntador=revistas;
-    for(int i=1;i<=2;i++){
-        cout<<"Nombre del usuario: ";
-        cin.getline(revistas[i].subscripcion.nombre,20);
-        cout<<"Tiempo de subscripcion: ";
-        cin>>revistas[i].subs;
-        cout<<"Nombre de la revista: ";
-        fflush(stdin);
-        cin.getline(revistas[i].subscripcion.rev,20);
-        cout<<"Costo de subscripcion: ";
-        cin>>revistas[i].costo,20;
-    }
-    cout<<endl;
-    for(;apuntador<=&revistas[1];apuntador++){
-        cout<<"Nombre del usuario: "<<apuntador->subscripcion.nombre<<endl;
-        cout<<"Tiempo de subscripcion: "<<apuntador->subs<<endl;
-        cout<<"Nombre de la revista: "<<apuntador->subscripcion.rev<<endl;
-        cout<<"Costo de subscripcion: "<<apuntador->costo<<endl;
-        cout<<endl;
+int main(){
+    char *locale;
+    locale=setlocale(LC_ALL,"");
+    struct persona P[10];
+    struct persona *apuntador;
+    apuntador=P;
+    int opcion,c;
+    char eliminar[20];
+    bool Repetir=true;
+    for(int i=0; i<10; i++)
+        P[i].nombre[0]='n';
+    while(Repetir==true){
+    	 cout<<"::M E N Ú::";
+		cout<<"¿Que desea realizar?"<<endl;
+		cout<<"\t1.-Alta"<<endl;
+		cout<<"\t2.-Eliminar"<<endl;
+		cout<<"\t3.-Imprimir"<<endl;
+		cout<<"\t4.-Buscar"<<endl;
+		cout<<"\t5.-Salir"<<endl;
+  		cin>>opcion;
+    switch (opcion){
+        case 1:
+            for(int i=0;i<10; i++){
+                if(P[i].nombre[0]=='n'){
+                    cout<<"Nombre del usuario: "; fflush(stdin);cin.getline(P[i].nombre,20,'\n');
+                    cout<<"Suscripcion: "; cin>>P[i].suscripcion;
+                    cout<<"Nombre de la revista: "; fflush(stdin); cin.getline(P[i].revista,20,'\n');
+                    cout<<'\n';
+                    break;
+                    }else
+                    cout<<"El lugar "<<i+1<<"º esta ocupado"<<endl;
+        }break;
+        case 2:
+            cout<<"Nombre del usuario que eliminaras: ";fflush(stdin);cin.getline(eliminar,20,'\n');
+            for(int i=0; i<10; i++){
+                c=strcmp(apuntador->nombre,eliminar);
+                if (c!=0)
+                    apuntador++;
+                else{
+                    P[i].nombre[0]='n';
+                    cout<<"Se ha dado de baja";
+                    break;
+                }
+            }
+            break;
+        case 3:
+            cout<<"\t Imprimido"<<endl;
+            for(int i=0; i<10; i++){
+                if (P[i].nombre[0]!='n'){
+                    cout<<"Nombre: "<<apuntador->nombre<<endl;
+                    cout<<"Suscripcion: "<<apuntador->suscripcion<<endl;
+                    cout<<"Nombre de la Revista:"<<apuntador->revista<<endl;
+                    apuntador++;
+                }
+                }
+        	break;
+        case 4:
+            cout<<"Usuario que desea buscar: ";fflush(stdin);cin.getline(eliminar,20,'\n');
+            for(int i=0; i<10; i++){
+                c=strcmp(apuntador->nombre,eliminar);
+                if (c!=0){
+                    if (i==9)
+                        cout<< "NO encontrado"<<endl;
+                apuntador++;}
+                else{
+                    cout<<"Usuario encontrado"<<endl;
+                    cout<<"Nombre: "<<apuntador->nombre<<endl;
+                    cout<<"Suscripcion: "<<apuntador->suscripcion<<endl;
+                    cout<<"Revista: "<<apuntador->revista<<endl;
+                    break;
+                }
+            }            break;
+        case 5:
+            cout<<"GOOD BYE"<<endl;
+            Repetir=false;
+            break;
+
     }
 
+    }
     return 0;
 }
-
